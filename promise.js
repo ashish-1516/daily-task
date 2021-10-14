fd48c6fe505369c5eaea2e9a6b86164ae0cecd44
ashish-1516 <91537208+ashish-1516@users.noreply.github.com>
2021-10-14 14:56:22 +0530

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,26 @@
+const msg = (count) =>
+new Promise((resolve,reject)=>{
+    if (count>=1) {
+        setTimeout(() => {
+        document.querySelector(".countdown").innerText = count;
+        resolve(count);
+    }, 1000);
+} else {
+    setTimeout(() => {
+        document.querySelector(".countdown").innerText = "Happy Independence Day";
+        reject("completed");
+    }, 1000);
+}
+});
+
+msg(9)
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.then((ct) => msg(-ct))
+.catch((err) => console.log(err));
